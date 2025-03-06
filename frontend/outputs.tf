f5a56c1e1706390c53493aca6fe1e25feb911663
Denis Gulev <denis_13@hotmail.it>
2025-03-06 14:30:44 +0100

    make outputs sensitive

--- frontend/outputs.tf
+++ frontend/outputs.tf
@@ -1,23 +1,28 @@
 output "dist_files" {
-  value = fileset(local.dist_dir, "**/*")
+  value     = fileset(local.dist_dir, "**/*")
+  sensitive = true
 }
 
 output "s3_website_endpoint" {
   description = "S3 hosting URL (HTTP)"
   value       = "http://${aws_s3_bucket_website_configuration.website_bucket.website_endpoint}"
+  sensitive   = true
 }
 
 output "cloudfront_id" {
   description = "Cloudfront ID"
   value       = aws_cloudfront_distribution.s3_distribution.id
+  sensitive   = true
 }
 
 output "cloudfront_url" {
   description = "Cloudfront distribution URL (HTTPS)"
   value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+  sensitive   = true
 }
 
 output "website_url" {
   description = "Website URL (HTTPS)"
   value       = "https://static-web.${var.domain_name}"
+  sensitive   = true
 }
