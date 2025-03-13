4dd7b2b3aebd4f2b7eb7cce61c8eb01f1cdaa03b
Denis Gulev <denis_13@hotmail.it>
2025-03-13 09:35:03 +0100

    removed unnecessary record

--- frontend/aws_route53_record.tf
+++ frontend/aws_route53_record.tf
@@ -27,18 +27,6 @@ resource "aws_route53_record" "api" {
   }
 }
 
-resource "aws_route53_record" "root_domain" {
-  zone_id = data.aws_route53_zone.existing.zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = true
-  }
-}
-
 resource "aws_route53_record" "www_redirect_static" {
   zone_id = data.aws_route53_zone.existing.zone_id
   name    = "www.${local.prefix}.${var.domain_name}"
