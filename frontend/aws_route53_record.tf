7b76fb4c13f94a3c99ef367def9c2194158e92f8
Denis Gulev <denis_13@hotmail.it>
2025-03-13 01:37:32 +0100

    trying to set Route53 records

--- frontend/aws_route53_record.tf
+++ frontend/aws_route53_record.tf
@@ -4,25 +4,53 @@ data "aws_route53_zone" "existing" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_record
-resource "aws_route53_record" "root_a" {
+resource "aws_route53_record" "static_website" {
   zone_id = data.aws_route53_zone.existing.zone_id
   name    = "${local.prefix}.${var.domain_name}"
   type    = "A"
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = true
   }
 }
 
-resource "aws_route53_record" "www_a" {
+resource "aws_route53_record" "api" {
   zone_id = data.aws_route53_zone.existing.zone_id
-  name    = "www.${local.prefix}.${var.domain_name}"
+  name    = "api.${var.domain_name}"
   type    = "A"
 
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
+    evaluate_target_health = true
   }
+}
+
+resource "aws_route53_record" "root_domain" {
+  zone_id = data.aws_route53_zone.existing.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "www_redirect" {
+  zone_id = data.aws_route53_zone.existing.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${var.domain_name}"]
+}
+
+resource "aws_route53_record" "www_redirect_static" {
+  zone_id = data.aws_route53_zone.existing.zone_id
+  name    = "www.${local.prefix}.${var.domain_name}"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["${local.prefix}.${var.domain_name}"]
 }
