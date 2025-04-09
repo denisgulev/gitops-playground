a0df7aed927d72913051a40fb40ebefaf44145ec
Denis Gulev <denis_13@hotmail.it>
2025-04-09 13:48:35 +0200

    escaping local constants

--- backend/user_data.sh
+++ backend/user_data.sh
@@ -3,7 +3,7 @@ set -e
 
 # Constants
 APP_DIR="/home/ec2-user"
-APP_FILE="${APP_DIR}/app.py"
+APP_FILE="$${APP_DIR}/app.py"
 SERVICE_FILE="/etc/systemd/system/flaskapp.service"
 NGINX_CONF="/etc/nginx/conf.d/flaskapp.conf"
 STATIC_ORIGIN="https://static-website.denisgulev.com"
@@ -20,7 +20,7 @@ source /etc/environment
 pip3 install flask gunicorn watchtower
 
 # Create Flask App
-cat > "$APP_FILE" <<EOF
+cat > "$$APP_FILE" <<EOF
 from flask import Flask, jsonify, request, redirect
 import watchtower, logging
 
@@ -38,14 +38,14 @@ def hello():
 @app.errorhandler(404)
 def page_not_found(e):
     LOGGER.warning(f"404 error: {request.path} not found.")
-    return redirect('${STATIC_ORIGIN}/error.html', code=302)
+    return redirect('$${STATIC_ORIGIN}/error.html', code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 EOF
 
 # Create Flask systemd service
-cat > "$SERVICE_FILE" <<EOF
+cat > "$$SERVICE_FILE" <<EOF
 [Unit]
 Description=Gunicorn instance to serve Flask app
 After=network.target
@@ -53,7 +53,7 @@ After=network.target
 [Service]
 User=ec2-user
 Group=ec2-user
-WorkingDirectory=${APP_DIR}
+WorkingDirectory=$${APP_DIR}
 EnvironmentFile=/etc/environment
 ExecStart=/usr/local/bin/gunicorn --workers 3 --bind 0.0.0.0:5000 app:app
 
@@ -66,7 +66,7 @@ systemctl daemon-reload
 systemctl enable --now flaskapp
 
 # Configure Nginx reverse proxy
-cat > "$NGINX_CONF" <<EOF
+cat > "$$NGINX_CONF" <<EOF
 server {
     listen 80;
     server_name ${api_domain};
@@ -78,7 +78,7 @@ server {
             access_log /var/log/nginx/options_requests.log;
             add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
             add_header 'Access-Control-Allow-Headers' 'Content-Type, Authorization' always;
-            add_header 'Access-Control-Allow-Origin' '${STATIC_ORIGIN}' always;
+            add_header 'Access-Control-Allow-Origin' '$${STATIC_ORIGIN}' always;
             add_header 'Access-Control-Max-Age' 1728000;
             add_header 'Content-Type' 'text/plain charset=UTF-8';
             add_header 'Content-Length' 0;
@@ -87,7 +87,7 @@ server {
 
         add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
         add_header 'Access-Control-Allow-Headers' 'Content-Type, Authorization' always;
-        add_header 'Access-Control-Allow-Origin' '${STATIC_ORIGIN}' always;
+        add_header 'Access-Control-Allow-Origin' '$${STATIC_ORIGIN}' always;
 
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
