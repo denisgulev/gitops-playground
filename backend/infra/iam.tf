989e454d570d64568f6c0fb1915de132b2a219f2
Denis Gulev <denis_13@hotmail.it>
2026-05-31 23:28:51 +0300

    add ssm param for instance id

--- backend/infra/iam.tf
+++ backend/infra/iam.tf
@@ -44,22 +44,9 @@ resource "aws_iam_role_policy" "cloudwatch_write" {
   })
 }
 
-resource "aws_iam_role" "prefix_list_access_role" {
-  name = "PrefixListAccessRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "prefix_list_access_profile" {
