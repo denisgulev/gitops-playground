4a21a62a872d8ec992a11e0769de83cc7123c3d9
Denis Gulev <denis_13@hotmail.it>
2026-05-31 23:35:07 +0300

    remove dead code

--- backend/infra/iam.tf
+++ backend/infra/iam.tf
@@ -48,27 +48,3 @@ resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
   role       = aws_iam_role.ec2_cloudwatch_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
-
-resource "aws_iam_instance_profile" "prefix_list_access_profile" {
-  name = "prefix-list-access-profile"
-  role = aws_iam_role.prefix_list_access_role.name
-}
-
-resource "aws_iam_role_policy" "prefix_list_access_policy" {
-  name = "prefix_list_access_policy"
-  role = aws_iam_role.prefix_list_access_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:DescribeManagedPrefixLists",
-          "ec2:GetManagedPrefixListEntries"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
