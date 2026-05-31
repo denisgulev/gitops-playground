4405cebc3ea0523823b2e71c8032fdb613504b57
Denis Gulev <denis_13@hotmail.it>
2026-05-31 23:39:16 +0300

    add ssm param for instance id (#50)
    * add ssm param for instance id
    
    * remove dead code

--- backend/infra/aws_ssm_parameter.tf
+++ backend/infra/aws_ssm_parameter.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "ec2_dns" {
   type  = "String"
   value = aws_eip.flask_app_eip.public_dns
 }
+
+resource "aws_ssm_parameter" "ec2_instance_id" {
+  name  = "/infra/ec2/instance_id"
+  type  = "String"
+  value = aws_instance.flask_app.id
+}
